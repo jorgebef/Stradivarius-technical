08cc6b2b189f43b68ed967d4ab3fb1b4991e07d0
Jorge Befan <jorge.befan@gmail.com>
2022-05-15 22:26:18 +0200

    create simpler way to view all meetups

--- src/pages/AllMeetupsPage.js
+++ src/pages/AllMeetupsPage.js
@@ -3,23 +3,24 @@ import { useFetch } from '../util-hooks/useFetch'
 import classes from './../components/meetups/MeetupList.module.css'
 
 export default function AllMeetupsPage() {
-
   // One single call to the data endpoint instead of calling for each card
   const { data } = useFetch({
-    url: '/data.json',
+    url: 'https://62813aef1020d8520586e92e.mockapi.io/meetups',
   })
+
+  if (!data) return <p>Loading...</p>
 
   return (
     <section>
       <h1>All Meetups</h1>
-      {data ? (
+      {data.length > 0 ? (
         <ul className={classes.list}>
           {data.map(item => (
-            <MeetupItem item={item} />
+            <MeetupItem key={item.id} item={item} />
           ))}
         </ul>
       ) : (
-        <p>Loading...</p>
+        <p>No Meetups</p>
       )}
     </section>
   )
